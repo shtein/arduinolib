687669ce2c8bcaf3c980d648d3989c935b82ecf9
Alex Shteyngolts <shtein@gmail.com>
2026-08-05 21:30:32 -0700

    ESPHOME native logging

--- include/DbgTool.h
+++ include/DbgTool.h
@@ -13,6 +13,24 @@
   #define DBG_OUT(FORMAT, ...)  _dbgOut(F(FORMAT), ##  __VA_ARGS__);     
   #define DBG_OUTLN(FORMAT, ...)  DBG_OUT(FORMAT "\n", ##  __VA_ARGS__); 
   
+#elif defined(ESPHOME_LOG_LEVEL)
+//For this we use ESPHome logging macros for debug output
+//Make sure your yaml has the following line to enable debug output:
+// esphome  
+//   platformio_options:  
+//     lib_ldf_mode: "chain"    
+//     lib_deps: 
+//       - https://github.com/shtein/arduinolib.git
+//     build_flags: 
+//       build_flags: 
+//         - -Isrc
+
+#include "esphome/core/log.h"
+
+#define DBG_INIT()
+#define DBG_OUT(FORMAT, ...)    ESP_LOGD("AL", FORMAT, ## __VA_ARGS__)
+#define DBG_OUTLN(FORMAT, ...)  ESP_LOGD("AL", FORMAT, ## __VA_ARGS__)
+  
 #else
   #define DBG_INIT() 
   #define DBG_OUT(...)
